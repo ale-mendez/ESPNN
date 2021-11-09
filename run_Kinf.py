e88e3e3806b38ed3524463842b1fdf2daeb7302f
MrRobot2211 <felipebihaiek@gmail.com>
2021-11-09 17:43:42 -0300

    add run_Kinf

---
+++ run_Kinf.py
@@ -0,0 +1,29 @@
+import numpy as np
+from .inference import run_inference
+
+
+def run_k_fold(folds, target_cols, NFOLDS, seed, device, verbose=False, **kwargs):
+    """Wraps run_inference to peform ensemble predictions"""
+
+    valid_df = folds
+
+    oof = np.zeros((len(valid_df), NFOLDS))
+
+    for fold in range(NFOLDS):
+
+        X_valid = valid_df[
+            [
+                "t_atomic_mass",
+                "projectile_mass",
+                "projectile_Z",
+                "Z_max",
+                "normalized_energy",
+                "target_ionisation",
+            ]
+        ].values
+
+        oof_ = run_inference(X_valid, fold, seed, verbose, **kwargs)
+
+        oof[:, fold] = oof_[:, 0]
+
+    return oof
