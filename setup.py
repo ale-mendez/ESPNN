ea9f3e21a0cea10e876c73e3fb6cce18af8b447d
Ale Mendez <alemdz.7@gmail.com>
2022-06-09 12:31:04 -0300

    bug in build

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="ESPNN",
-    version="0.1.0",
+    version="0.1.1",
     description="Electronic Stoping Power Neural Network predictor",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
@@ -65,7 +65,9 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
     ],
 )
