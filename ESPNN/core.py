2c4aa61de31615e672fd7cc1f602c799b35c744d
Ale Mendez <alemdz.7@gmail.com>
2022-09-13 20:40:54 -0300

    kfold variance to df and plot

--- ESPNN/core.py
+++ ESPNN/core.py
@@ -19,7 +19,8 @@ exp_name = "try0__00_en_ioen_0_bhe_corrected_trtestsplit_tuple_reduxdrop_nobias_
 dir_path = os.path.dirname(os.path.realpath(__file__))
 out_cols = {
     "E": "Energy (MeV/amu)",
-    "SP": "Stopping power (MeV cm2/mg)"
+    "SP": "Stopping power (MeV cm2/mg)",
+    "V": "variance"
 }
 
 
@@ -101,6 +102,7 @@ def run_NN(
         df_log[f"pred_{fold}"] = oof_[:, fold]
 
     df["stopping_power"] = np.mean(oof_, axis=1)
+    df["varianza"] = np.var(oof_, axis=1)
     df["system"] = df["projectile"] + "_" + df["target"]
     for tup in df["system"].unique():
         df_tup = df.loc[df["system"] == tup]
@@ -113,7 +115,8 @@ def run_NN(
     df_out = pd.DataFrame(
         {
             out_cols["E"]: df_tup["normalized_energy"],
-            out_cols["SP"]: df_tup["stopping_power"]
+            out_cols["SP"]: df_tup["stopping_power"],
+            out_cols["V"]: df_tup["variance"]
         }
     )
 
@@ -137,9 +140,11 @@ def run_NN(
 def plot_prediction(projectile, target, df):
     e = out_cols['E']
     sp = out_cols['SP']
+    v = out_cols['V']
     title = ' '.join([projectile, "on", target])
     fig, ax = plt.subplots(1, 1, figsize=(8 * 1.1, 6 * 1.1))
     ax.scatter(df[e], df[sp], marker='.')
+    ax.fill_between(df[e], df[sp] - df[v], df[sp] + df[v], color='tab:blue', alpha=0.3)
     ax.set_title(title, fontsize=20)
     ax.set_xscale("log")
     ax.set_xlabel(r"Energy (MeV/amu)", fontsize=18)
